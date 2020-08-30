3c6fca56e067dca5961f04070c026dc9b61940b7
chipytux <chipytux@gmail.com>
2020-08-30 16:32:31 -0300

    Solução Desafio 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRespositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `${Date.now()}`,
+      url: "https://github.com/chipytux/go-stack-desafio-2",
+      techs: ["NODEJS"],
+    });
+
+    setRespositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then(() => {
+      setRespositories(repositories.filter((rep) => rep.id !== id));
+    });
   }
+
+  useEffect(() => {
+    api.get("repositories").then((res) => {
+      setRespositories(res.data);
+    });
+  }, []);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
